081cb1779847ecf99cb109b0f2a08dbeddb4e4ac
whuanle <1586052146@qq.com>
2018-10-14 18:52:15 +0800

    2018-10-15,19:00

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -19,9 +19,38 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
         {
-            return View(await _context.Movie.ToListAsync());
+            return "From [HttpPost]Index: filter on " + searchString;
+        }
+
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
+        {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel();
+            movieGenreVM.genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            movieGenreVM.movies = await movies.ToListAsync();
+
+            return View(movieGenreVM);
         }
 
         // GET: Movies/Details/5
@@ -29,17 +58,17 @@ namespace MvcMovie.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return NotFound();  //返回404
             }
 
             var movie = await _context.Movie
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (movie == null)
+                .FirstOrDefaultAsync(m => m.ID == id);     //返回按id条件查找结果中的第一个元素(一条结果)
+            if (movie == null)   //按id查找不到结果
             {
-                return NotFound();
+                return NotFound();  //返回404
             }
 
-            return View(movie);
+            return View(movie);   //如果查找成功，就向视图传递数据模型
         }
 
         // GET: Movies/Create
--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
         public string Title { get; set; }
-        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+
+        [Display(Name ="Release Date")]            //格式化数据，不提供验证功能，在使用到的地方会自动格式化
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         [DataType(DataType.Currency)]
+        [Column(TypeName ="decimal(18,2)")]
         public decimal Price { get; set; }
+    }
+    public class MovieGenreViewModel
+    {
+        public List<Movie> movies;
+        public SelectList genres;
+        public string movieGenre { get; set; }
     }
 }
