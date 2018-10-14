65cf54c0c1ceb0355b5bdb00fbeb22932f444ec3
whuanle <1586052146@qq.com>
2018-10-14 15:21:14 +0800

    添加项目文件。

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class SeedData           //数据库种子
+    {
+        public static void Initialize(IServiceProvider serviceProvider)  //实例化连接
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+            {
+                if (context.Movie.Any())  //如果数据库存在数据，则不添加种子
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(           //不存在数据，则添加数据
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-1-11"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
